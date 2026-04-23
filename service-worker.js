dab3eb7d491cf2df7ecfdcda4b84dfdc6d0b39f5
petris66 <128893373+petris66@users.noreply.github.com>
2026-04-23 14:17:20 +0300

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js
+const CACHE_NAME = "minun-sovellus-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
