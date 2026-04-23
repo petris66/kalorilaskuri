0b95a6ad7812350a62e9f05d974158fdc820a14f
petris66 <128893373+petris66@users.noreply.github.com>
2026-04-23 18:56:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,8 @@
-const CACHE_NAME = "kalorilaskuri-v4";
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting(); // pakottaa uuden version heti käyttöön
+// Poistetaan vanha välimuisti ja lopetetaan service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    ).then(() => self.clients.claim()) // ottaa hallinnan heti ilman sivun latausta
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
